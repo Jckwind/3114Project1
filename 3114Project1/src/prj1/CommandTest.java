cc5327898087a1ceb96cd13d7e4cda4916c26f1b
Gannon <Gannon@DESKTOP-Q0ROKRT>
2020-09-29 11:12:45 -0400

    CommandTest commit

---
+++ 3114Project1/src/prj1/CommandTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package prj1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Gannon
+ *
+ */
+class CommandTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
